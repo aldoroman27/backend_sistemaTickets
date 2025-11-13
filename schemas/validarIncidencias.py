19e345e2ee6b36069d1b435c100e234123374c28
aldoroman27 <juggeraldo@gmail.com>
2025-11-13 14:46:55 -0600

    Hicimos la adición de nuevos endpoints

--- schemas/validarIncidencias.py
+++ schemas/validarIncidencias.py
@@ -4,9 +4,9 @@ from marshmallow import Schema, fields
 class validarIncidenciaSchema(Schema):
     idEmpleado = fields.String(required=True)
     nombreCompleto = fields.String(required=True)
-    puesto = fields.String(requiered=True)
-    correoElectronico = fields.Email(required=True)
+    puesto = fields.String(required=True)
     departamento = fields.String(required=True)
     justificacion = fields.String(required=True)
     fecha = fields.Date(required=True)
-    tipoIncidencia = fields.Date(required=True)
+    tipoIncidencia = fields.String(required=True)
+    estado = fields.String(required=True)
